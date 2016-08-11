7e3e8c13f8742f0c4669f716189a064ac4d0bb54
madeleinehuish <vasistha@hotmail.com>
2016-08-11 11:50:29 -0700

    add yell.js

---
+++ yell.js
@@ -0,0 +1,13 @@
+
+function yell(){
+  for (i = 1; i <= 10; i++){
+    if (i < 10)
+    {console.log('this is the phrase');
+  }
+    else {
+      console.log('THIS IS THE PRASE');
+    }
+  }
+}
+
+yell();
