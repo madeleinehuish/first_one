23116579167f908c0eb4e09b2ba712a25fe7b517
madeleinehuish <vasistha@hotmail.com>
2016-08-12 08:37:30 -0700

    retry

--- daysOfMonth.js
+++ daysOfMonth.js
@@ -1,18 +1,22 @@
 function determineDaysOfMonth(month) {
-    if (month === "January" || "March" || "May" || "July" || "August" || "October" || "December") {
+    console.log("month inside determineDaysofMonth " + month);
+    if (month === "January" || month === "March" || month === "May" || month === "July" || month === "August" || month === "October" || month === "December") {
         var days = 31;
         return days;
-    } else if (month === "April" || "June" || "September" || "November") {
+    } else if (month === "April" || month === "June" || month === "September" || month === "November") {
+        console.log(30);
         var days = 30;
         return days;
-    } else  {
+    } else {
+        console.log(28);
         var days = 28;
         return days;
     }
 }
 
-function printDaysOfMonth(days, month) {
-    for (i = 1; i < days + 1; i++) {
+function printDaysOfMonth(days, month){
+    console.log("days inside printDaysOfMonth " + days);
+    for (i = 1; i < days +1; i++) {
         console.log(month + " " + i);
     }
 }
